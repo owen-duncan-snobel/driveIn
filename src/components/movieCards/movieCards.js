8bb2b3efc425ed1c601560f5c4eb5061c80a20af
owen-duncan-snobel <oadster@cse.yorku.ca>
2020-07-26 21:00:05 -0400

    tempSave

--- src/components/movieCards/movieCards.js
+++ src/components/movieCards/movieCards.js
@@ -6,43 +6,34 @@ function MovieCards(props) {
     return (
         <div>
             {screens.map(movie => {
-
                 let movies = screens.filter((el) => el.screenNumber === movie.screenNumber);
 
                 return (
-                    <div className="h-screen w-screen" >
+                    <div className="flex flex-wrap overflow-hidden">
+                        {movies.map(movie => {
+                            return (
+                                <div className="w-full overflow-hidden sm:w-1/2 bg-red-500 border">
+                                    <img className="w-full max-w-sm object-cover mx-auto" src={movie.movie.moviePoster}></img>
+                                    <div className="px-6 py-4">
+                                        <div className="font-bold text-xl mb-2">{movie.movie.movieName}</div>
+                                        <p className="text-gray-700 text-base">
+                                            {movie.screenTime}
+                                        </p>
+                                        <p className="text-gray-700 text-base">
+                                            {movie.movie.rating}
+                                        </p>
+                                    </div>
+                                    <div className="px-6 py-4">
+                                        <span>
+                                            <button>BUY TICKET</button>
+                                            <button>TRAILER</button>
+                                        </span>
+                                    </div>
+                                </div>
 
-                        <div className="max-w-5xl mx-auto p-10 space-y-5">
-                            <h1>{movie.screenNumber}</h1>
-                            <div className="grid sm:grid-cols-2 gap-5">
-
-                                {movies.map(movie => {
-                                    return (
-                                        <div className="grid max-w-sm rounded overflow-hidden shadow-lg m-20">
-
-                                            <img className="w-full" src={movie.movie.moviePoster}></img>
-                                            <div className="px-6 py-4">
-                                                <div className="font-bold text-xl mb-2">{movie.movie.movieName}</div>
-                                                <p className="text-gray-700 text-base">
-                                                    {movie.screenTime}
-                                                </p>
-                                                <p className="text-gray-700 text-base">
-                                                    {movie.movie.rating}
-                                                </p>
-                                            </div>
-                                            <div className="px-6 py-4">
-                                                <span>
-                                                    <button>BUY TICKET</button>
-                                                    <button>TRAILER</button>
-                                                </span>
-                                            </div>
-                                        </div>
-
-                                    )
-                                })}
-                            </div>
-                        </div>
-                    </div >
+                            )
+                        })}
+                    </div>
                 )
             })}
         </div>
