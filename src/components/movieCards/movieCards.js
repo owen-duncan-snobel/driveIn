91ceba9a7a356a65be21cb5e716d4bf4c60dbd6b
owen-duncan-snobel <oadster@cse.yorku.ca>
2020-07-26 21:42:46 -0400

    v1.2 movie card layout, correct breakpoints and sizing. Gap is working correctly

--- src/components/movieCards/movieCards.js
+++ src/components/movieCards/movieCards.js
@@ -9,30 +9,35 @@ function MovieCards(props) {
                 let movies = screens.filter((el) => el.screenNumber === movie.screenNumber);
 
                 return (
-                    <div className="flex flex-wrap overflow-hidden lg:w-1/2 mx-auto">
-                        {movies.map(movie => {
-                            return (
-                                <div className="w-full overflow-hidden sm:w-1/2 mx-auto border">
-                                    <img className="w-full max-w-sm object-cover mx-auto" src={movie.movie.moviePoster}></img>
-                                    <div className="px-6 py-4">
-                                        <div className="font-bold text-xl mb-2">{movie.movie.movieName}</div>
-                                        <p className="text-gray-700 text-base">
-                                            {movie.screenTime}
-                                        </p>
-                                        <p className="text-gray-700 text-base">
-                                            {movie.movie.rating}
-                                        </p>
-                                    </div>
-                                    <div className="px-6 py-4">
-                                        <span>
-                                            <button>BUY TICKET</button>
-                                            <button>TRAILER</button>
-                                        </span>
-                                    </div>
-                                </div>
+                    <div>
+                        <p className="font-sans text-6xl text-center">Screen {movie.screenNumber}</p>
+                        <div className="flex flex-wrap overflow-hidden">
+                            <div className="block sm:flex sm:flex-wrap  overflow-hidden mx-auto gap-2">
+                                {movies.map(movie => {
+                                    return (
+                                        <div className="overflow-hidden border rounded-lg mx-auto">
+                                            <img className="w-full max-w-sm object-cover" src={movie.movie.moviePoster}></img>
+                                            <div className="px-6 py-4">
+                                                <div className="font-bold text-xl mb-2">{movie.movie.movieName}</div>
+                                                <p className="text-gray-700 text-base">
+                                                    {movie.screenTime}
+                                                </p>
+                                                <p className="text-gray-700 text-base">
+                                                    {movie.movie.rating}
+                                                </p>
+                                            </div>
+                                            <div className="px-6 py-4">
+                                                <span>
+                                                    <button>BUY TICKET</button>
+                                                    <button>TRAILER</button>
+                                                </span>
+                                            </div>
+                                        </div>
 
-                            )
-                        })}
+                                    )
+                                })}
+                            </div>
+                        </div>
                     </div>
                 )
             })}
