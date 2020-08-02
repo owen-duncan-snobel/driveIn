2f3b33bcf13871b8eaaef507eb5e90a0f94958f4
owen-duncan-snobel <oadster@cse.yorku.ca>
2020-08-02 03:05:48 -0400

    need to add grouping (filtering) by the screen date. Equal dates then by screen numbers

--- src/components/movieCards/movieCards.js
+++ src/components/movieCards/movieCards.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import moment from 'moment';
 
 function MovieCards(props) {
     const screens = props.screens;
-    console.log(screens)
+
     try {
         return (
             <div>
                 {/** For Every Screen Filter by screen each movie at the specific screen */}
                 {screens.map(movie => {
-                    let movies = screens.filter((el) => el.screenNumber === movie.screenNumber);
+
+                    // * Filter so that the screenNumber is === to the movieScreen number 
+                    let movies = screens.filter((screen) => screen.screenNumber === movie.screenNumber);
+                    // * Sort so that the times for the screen are in ascending order based on the times.
+                    movies.sort((a, b) => a.screenTime > b.screenTime);
                     return (
                         <div>
                             <p className="font-bold text-5xl text-center text-gray-900">Screen {movie.screenNumber}</p>
@@ -18,16 +23,25 @@ function MovieCards(props) {
                                     {movies.map(movie => {
                                         let url = 'http://localhost:1337'
                                         let movieData = movie.movie[0];
+                                        let screenTime = movie.screenTime;
+
+                                        try {
+                                            // * Convert the time to the standard 12 hour clock format
+                                            screenTime = moment(screenTime, 'hh:mm:ss.SSS').format('h:mm a')
+                                        } catch (e) {
+                                            console.log(e);
+                                        }
 
                                         return (
-                                            <div className="overflow-hidden border rounded-lg mx-auto">
+                                            <div className="overflow-hidden border rounded-lg mx-auto relative">
                                                 <img className="w-full max-w-sm object-cover" src={url + movieData.moviePoster.url} alt={movieData.movieName}></img>
                                                 <div className="flex">
+                                                    <div className="mt-24 relative"></div>
                                                     <div className="w-1/2">
-                                                        <div className="px-6 py-4">
+                                                        <div className="px-6 py-4 absolute bottom-0">
                                                             <div className="font-bold text-xl mb-2" aria-label={movieData.movieName}>{movieData.movieName}</div>
                                                             <p className="text-gray-700 text-base">
-                                                                {movie.screenTime.substring(0, 5)}
+                                                                {screenTime}
                                                             </p>
                                                             <p className="text-gray-700 text-base">
                                                                 {movie.rating}
@@ -36,7 +50,7 @@ function MovieCards(props) {
                                                     </div>
 
                                                     <div className="w-1/2 pt-10">
-                                                        <div className="px-6 py-4">
+                                                        <div className="px-6 py-4 absolute bottom-0">
                                                             <span>
                                                                 <button className="" aria-label="BUY TICKET">BUY TICKET</button>
                                                                 <button className="" aria-label="TRAILER">TRAILER</button>
