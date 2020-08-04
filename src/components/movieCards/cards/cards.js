a72283b7d6c4db0f13450035d45d2bb1badd45d1
owen-duncan-snobel <oadster@cse.yorku.ca>
2020-08-04 00:23:01 -0400

    Logic for the movie cards / screens / cards updated to be smaller usable components

---
+++ src/components/movieCards/cards/cards.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function Cards(props) {
+    let movie = props.screen.movie;
+    let screenTime = props.screenTime;
+    let url = 'http://localhost:1337';
+
+    return (
+        <div className="overflow-hidden border rounded-lg mx-auto relative">
+            <img className="w-full max-w-sm object-cover" src={url + movie.moviePoster.url} alt={movie.movieName}  ></img>
+            <div className="flex">
+                <div className="mt-24 relative"></div>
+                <div className="w-1/2">
+                    <div className="px-6 py-4 absolute bottom-0">
+                        <div className="font-bold text-xl mb-2" aria-label={movie.movieName} > {movie.movieName} </div>
+                        <p className="text-gray-700 text-base">
+                            {screenTime}
+                        </p>
+                        <p className="text-gray-700 text-base">
+                            {movie.rating}
+                        </p>
+                    </div>
+                </div>
+
+                <div className="w-1/2 pt-10">
+                    <div className="px-6 py-4 absolute bottom-0">
+                        <span>
+                            <button className="" aria-label="BUY TICKET">BUY TICKET</button>
+                            <button className="" aria-label="TRAILER">TRAILER</button>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+} export default Cards;
--- src/components/movieCards/movieCards.js
+++ src/components/movieCards/movieCards.js
@@ -1,75 +1,60 @@
 import React from 'react';
+import _ from 'lodash';
 import moment from 'moment';
+import Screens from './screens/screens';
 
 function MovieCards(props) {
-    const screens = props.screens;
+
+
+    // * All Screens is all the entries that have been fetched from the DB Table Screens
+    const allScreens = props.screens;
+    console.log(allScreens)
+    // * Sort then Group all the screens together by date in ascending order
+    const screenDates = _.chain(allScreens)
+        .sort((a, b) => a.screenDate > b.screenDate)
+        .groupBy(screen => screen.screenDate)
+        .entries()
+        .value()
 
     try {
         return (
             <div>
-                {/** For Every Screen Filter by screen each movie at the specific screen */}
-                {screens.map(movie => {
+                {/** For Each Date, select the Screens that have Movies playing on that date  */}
+                {screenDates.map(date => {
+                    // * index 0 holds the Date, index 1 has the Screens with movies on the date
+                    let screens = date[1];
+                    let screenDate = moment(date[0], 'YYYY-MM-DD').toLocaleString().substr(0, 15);
 
-                    // * Filter so that the screenNumber is === to the movieScreen number 
-                    let movies = screens.filter((screen) => screen.screenNumber === movie.screenNumber);
-                    // * Sort so that the times for the screen are in ascending order based on the times.
-                    movies.sort((a, b) => a.screenTime > b.screenTime);
+                    const screensOnDate = _.chain(screens)
+                        .sort((a, b) => a.screenTime > b.screenTime)
+                        .groupBy(screen => screen.screenNumber)
+                        .entries()
+                        .value()
+
+                    // * For Every Screen on the specific date you need to display all movies on each screen
                     return (
                         <div>
-                            <p className="font-bold text-5xl text-center text-gray-900">Screen {movie.screenNumber}</p>
-                            <div className="flex flex-wrap overflow-hidden">
-                                <div className="block sm:flex sm:flex-wrap  overflow-hidden mx-auto gap-2">
-                                    {/* For Every Movie at the screen create its card with the information from db */}
-                                    {movies.map(movie => {
-                                        let url = 'http://localhost:1337'
-                                        let movieData = movie.movie[0];
-                                        let screenTime = movie.screenTime;
+                            <p className="font-bold text-5xl text-center text-gray-900">{screenDate}</p>
+                            {screensOnDate.map(screens => {
+                                let screenNumber = screens[0];
+                                return (
+                                    <div>
+                                        <p className="font-bold text-5xl text-center text-gray-900">Screen {screenNumber}</p>
 
-                                        try {
-                                            // * Convert the time to the standard 12 hour clock format
-                                            screenTime = moment(screenTime, 'hh:mm:ss.SSS').format('h:mm a')
-                                        } catch (e) {
-                                            console.log(e);
-                                        }
-
-                                        return (
-                                            <div className="overflow-hidden border rounded-lg mx-auto relative">
-                                                <img className="w-full max-w-sm object-cover" src={url + movieData.moviePoster.url} alt={movieData.movieName}></img>
-                                                <div className="flex">
-                                                    <div className="mt-24 relative"></div>
-                                                    <div className="w-1/2">
-                                                        <div className="px-6 py-4 absolute bottom-0">
-                                                            <div className="font-bold text-xl mb-2" aria-label={movieData.movieName}>{movieData.movieName}</div>
-                                                            <p className="text-gray-700 text-base">
-                                                                {screenTime}
-                                                            </p>
-                                                            <p className="text-gray-700 text-base">
-                                                                {movie.rating}
-                                                            </p>
-                                                        </div>
-                                                    </div>
-
-                                                    <div className="w-1/2 pt-10">
-                                                        <div className="px-6 py-4 absolute bottom-0">
-                                                            <span>
-                                                                <button className="" aria-label="BUY TICKET">BUY TICKET</button>
-                                                                <button className="" aria-label="TRAILER">TRAILER</button>
-                                                            </span>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        )
-                                    })}
-                                </div>
-                            </div>
+                                        <Screens screens={screens} />
+                                    </div>
+                                )
+                            })}
                         </div>
                     )
-                })}
-            </div>
+                })
+                }
+            </div >
         )
     } catch (e) {
         console.log(e);
-        return (<div></div>);
+        return (
+            <div></div>
+        );
     }
 } export default MovieCards;
