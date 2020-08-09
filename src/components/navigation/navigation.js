5ed225f73739e0b69ea7e34f1412506f35603201
owen-duncan-snobel <oadster@cse.yorku.ca>
2020-08-08 22:13:51 -0400

    responsive menu button working at mobile breakpoints

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -7,45 +7,124 @@ import Prices from '../../routes/prices/prices';
 
 function Navigation() {
 
-    /*     const [navbarOpen, setNavbarOpen] = React.useState(false);
-    
-        const [activePage, setActivePage] = React.useState(false);
+    const [navbarOpen, setNavbarOpen] = React.useState(false);
+    /*
+       
     
         const toggleClass = () => {
         } */
 
     return (
         <Router>
-            <nav className="flex items-center justify-between flex-wrap p-6">
-                <div className="flex items-center flex-shrink-0  mr-6">
-                    <NavLink exact to="/">
-                        <span className="font-bold text-4xl tracking-tight bg-indigo-400 p-3">Drive in</span>
-                    </NavLink>
+            <nav>
+                {/** Medium breakpoint and up Menu Bar */}
+                <div className="flex items-center justify-between md:justify-start">
+                    <div className="p-6">
+                        <NavLink exact to="/">
+                            <span className="font-bold text-4xl tracking-tight bg-indigo-400 p-3">Drive in</span>
+                        </NavLink>
+                    </div>
+
+                    <div className="md:hidden m-6">
+                        <button onClick={() => setNavbarOpen(!navbarOpen)}>
+                            <svg className="stroke-current text-gray-900 w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                <line x1="3" y1="12" x2="21" y2="12"></line>
+                                <line x1="3" y1="6" x2="21" y2="6">
+                                </line><line x1="3" y1="18" x2="21" y2="18"></line>
+                            </svg>
+                        </button>
+                    </div>
+
+                    <div className="text-sm font-semibold hidden md:block justify-center">
+                        <NavLink exact to="/nowplaying" className="block  sm:inline-block lg:mt-0 mr-4 text-gray-500 hover:text-gray-900" activeClassName="text-gray-900" >
+                            Now Playing
+                            </NavLink>
+
+                        <NavLink exact to="/comingsoon" className="block  sm:inline-block lg:mt-0 mr-4  text-gray-500 hover:text-gray-900" activeClassName="text-gray-900" >
+                            Coming Soon
+                            </NavLink>
+
+                        <NavLink exact to="/buytickets" className="block  sm:inline-block lg:mt-0 mr-4 text-gray-500 hover:text-gray-900" activeClassName="text-gray-900" >
+                            Buy Tickets
+                            </NavLink>
+
+                        <NavLink exact to="/food" className="block  sm:inline-block lg:mt-0 mr-4 text-gray-500 hover:text-gray-900" activeClassName="text-gray-900" >
+                            Food
+                            </NavLink>
+
+                        <NavLink exact to="/theatreinfo" className="block  sm:inline-block lg:mt-0 mr-4 text-gray-500 hover:text-gray-900" activeClassName="text-gray-900" >
+                            Theatre Info
+                            </NavLink>
+
+                        <NavLink exact to="/prices" className="block  sm:inline-block lg:mt-0 mr-4 text-gray-500 hover:text-gray-900 " activeClassName="text-gray-900" >
+                            Prices
+                            </NavLink>
+                    </div>
+
                 </div>
 
-                <div className="w-full block flex-grow md:flex sm:w-auto ">
-                    <div className="text-sm font-semibold hidden md:block">
-                        <NavLink exact to="/nowplaying" className="block mt-4 sm:inline-block lg:mt-0 mr-4 text-gray-500 hover:text-gray-900" activeClassName="text-gray-900" >
-                            Now Playing
-                    </NavLink>
-                        <NavLink exact to="/comingsoon" className="block mt-4 sm:inline-block lg:mt-0 mr-4  text-gray-500 hover:text-gray-900" activeClassName="text-gray-900" >
-                            Coming Soon
-                    </NavLink>
-                        <NavLink exact to="/buytickets" className="block mt-4 sm:inline-block lg:mt-0 mr-4 text-gray-500 hover:text-gray-900" activeClassName="text-gray-900" >
-                            Buy Tickets
-                    </NavLink>
-                        <NavLink exact to="/food" className="block mt-4 sm:inline-block lg:mt-0 mr-4 text-gray-500 hover:text-gray-900" activeClassName="text-gray-900" >
-                            Food
-                    </NavLink>
-                        <NavLink exact to="/theatreinfo" className="block mt-4 sm:inline-block lg:mt-0 mr-4 text-gray-500 hover:text-gray-900" activeClassName="text-gray-900" >
-                            Theatre Info
-                    </NavLink>
-                        <NavLink exact to="/prices" className="block mt-4 sm:inline-block lg:mt-0 mr-4 text-gray-500 hover:text-gray-900 " activeClassName="text-gray-900" >
-                            Prices
-                    </NavLink>
-                    </div>
+                {/** MOBILE MENU BAR */}
+                <div className={"text-sm  font-semibold justify md:hidden pl-5 " + (navbarOpen ? 'block' : 'hidden')}>
+                    <NavLink exact to="/nowplaying" className="block lg:mt-0 mr-4 text-gray-500 hover:text-gray-900" activeClassName="text-gray-900" >
+                        Now Playing
+                            </NavLink>
+
+                    <NavLink exact to="/comingsoon" className="block  lg:mt-0 mr-4  text-gray-500 hover:text-gray-900" activeClassName="text-gray-900" >
+                        Coming Soon
+                            </NavLink>
+
+                    <NavLink exact to="/buytickets" className="block lg:mt-0 mr-4 text-gray-500 hover:text-gray-900" activeClassName="text-gray-900" >
+                        Buy Tickets
+                            </NavLink>
+
+                    <NavLink exact to="/food" className="block lg:mt-0 mr-4 text-gray-500 hover:text-gray-900" activeClassName="text-gray-900" >
+                        Food
+                            </NavLink>
+
+                    <NavLink exact to="/theatreinfo" className="block lg:mt-0 mr-4 text-gray-500 hover:text-gray-900" activeClassName="text-gray-900" >
+                        Theatre Info
+                            </NavLink>
+
+                    <NavLink exact to="/prices" className="block lg:mt-0 mr-4 text-gray-500 hover:text-gray-900 " activeClassName="text-gray-900" >
+                        Prices
+                            </NavLink>
                 </div>
             </nav>
+            {/* <nav className="flex flex-wrap p-6 justify-between">
+                <NavLink exact to="/">
+                    <span className="font-bold text-4xl tracking-tight bg-indigo-400 p-3">Drive in</span>
+                </NavLink>
+
+                
+                <div className={"flex-grow items-center " + (navbarOpen ? "flex" : "hidden")} >
+                    <div className="md:flex sm:w-auto sm:hidden">
+                        <div className="text-sm font-semibold hidden md:block ml-4">
+                            <NavLink exact to="/nowplaying" className="block  sm:inline-block lg:mt-0 mr-4 text-gray-500 hover:text-gray-900" activeClassName="text-gray-900" >
+                                Now Playing
+                                </NavLink>
+                            <NavLink exact to="/comingsoon" className="block  sm:inline-block lg:mt-0 mr-4  text-gray-500 hover:text-gray-900" activeClassName="text-gray-900" >
+                                Coming Soon
+                                </NavLink>
+                            <NavLink exact to="/buytickets" className="block  sm:inline-block lg:mt-0 mr-4 text-gray-500 hover:text-gray-900" activeClassName="text-gray-900" >
+                                Buy Tickets
+                                </NavLink>
+                            <NavLink exact to="/food" className="block  sm:inline-block lg:mt-0 mr-4 text-gray-500 hover:text-gray-900" activeClassName="text-gray-900" >
+                                Food
+                                </NavLink>
+                            <NavLink exact to="/theatreinfo" className="block  sm:inline-block lg:mt-0 mr-4 text-gray-500 hover:text-gray-900" activeClassName="text-gray-900" >
+                                Theatre Info
+                                </NavLink>
+                            <NavLink exact to="/prices" className="block  sm:inline-block lg:mt-0 mr-4 text-gray-500 hover:text-gray-900 " activeClassName="text-gray-900" >
+                                Prices
+                                </NavLink>
+                        </div>
+                    </div>
+                </div>
+
+                <button className="md:hidden text-right" onClick={() => setNavbarOpen(!navbarOpen)}>
+                    <svg className="w-8 h-8" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                </button>
+            </nav> */}
             <Switch>
                 <Route path="/comingsoon">
                     <ComingSoon />
