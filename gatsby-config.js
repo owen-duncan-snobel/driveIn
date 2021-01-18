2fc6d7e5fdab1b2150abf719ffdb51ad11d775a8
owen-duncan-snobel <oadster@cse.yorku.ca>
2021-01-18 06:06:43 -0500

    attempting to fix ssr errors on gatsby build

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
 require("dotenv").config({
   path: `.env`,
 })
-
 module.exports = {
   siteMetadata: {
     title: `Drive In (Gatsby, Strapi, TailwindCSS)`,
--- src/components/movieCards/cards/cards.js
+++ src/components/movieCards/cards/cards.js
@@ -1,43 +1,47 @@
 import React from "react"
+import NoSSR from "react-no-ssr"
 
 const Cards = ({ screen, screenTime }) => {
   let movie = screen.movie
+  console.log(movie)
   return (
-    <div className="overflow-hidden border rounded-lg mx-auto relative m-2 shadow-xs">
-      <img
-        className="w-full max-w-sm h-64 object-cover pb-2"
-        src={movie.moviePoster.publicURL}
-        alt={movie.movieName + " Poster"}
-      ></img>
-      <div className="flex">
-        <div className="mt-24 relative"></div>
-        <div className="w-1/2">
-          <div className="px-6 py-4 absolute bottom-0">
-            <div
-              className="font-bold text-xl mb-2"
-              aria-label={movie.movieName}
-            >
-              {movie.movieName}
+    <NoSSR>
+      <div className="overflow-hidden border rounded-lg mx-auto relative m-2 shadow-xs">
+        <img
+          className="w-full max-w-sm h-64 object-cover pb-2"
+          src={movie.moviePoster.publicURL}
+          alt={movie.movieName + " Poster"}
+        ></img>
+        <div className="flex">
+          <div className="mt-24 relative"></div>
+          <div className="w-1/2">
+            <div className="px-6 py-4 absolute bottom-0">
+              <div
+                className="font-bold text-xl mb-2"
+                aria-label={movie.movieName}
+              >
+                {movie.movieName}
+              </div>
+              <p className="text-gray-700 text-base">{screenTime}</p>
+              <p className="text-gray-700 text-base">{movie.rating}</p>
             </div>
-            <p className="text-gray-700 text-base">{screenTime}</p>
-            <p className="text-gray-700 text-base">{movie.rating}</p>
           </div>
-        </div>
 
-        <div className="w-1/2 pt-10">
-          <div className="px-6 py-4 absolute bottom-0">
-            <span>
-              <button className="" aria-label="BUY TICKET">
-                BUY TICKETS
-              </button>
-              <button className="" aria-label="TRAILER">
-                TRAILER
-              </button>
-            </span>
+          <div className="w-1/2 pt-10">
+            <div className="px-6 py-4 absolute bottom-0">
+              <span>
+                <button className="" aria-label="BUY TICKET">
+                  BUY TICKETS
+                </button>
+                <button className="" aria-label="TRAILER">
+                  TRAILER
+                </button>
+              </span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </NoSSR>
   )
 }
 export default Cards
