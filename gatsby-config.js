61aa25c42da47a8f3b83607abd34d43b804e11a9
owen-duncan-snobel <oadster@cse.yorku.ca>
2021-01-17 19:37:07 -0500

    gatsby files added for refactoring

---
+++ gatsby-config.js
@@ -0,0 +1,58 @@
+require("dotenv").config({
+  path: `.env`,
+})
+
+module.exports = {
+  siteMetadata: {
+    title: `Drive In (Gatsby, Strapi, TailwindCSS)`,
+    description: ``,
+    author: `Owen Duncan-Snobel`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: "gatsby-source-strapi",
+      options: {
+        apiURL: process.env.API_URL || "http://localhost:1337",
+        contentTypes: ["movies", "screens", "prices", "theatres"],
+        queryLimit: 1000,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    "gatsby-plugin-offline",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/drivein.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-postcss`,
+      options: {
+        postCssPlugins: [require("tailwindcss")],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true,
+        develop: false,
+        tailwind: true,
+      },
+    },
+  ],
+}
